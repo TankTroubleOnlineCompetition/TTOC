f5ed43b6a6694dd4375f2f26a6e573157cb0d47b
Commander <asger.finding@gmail.com>
2021-08-10 10:03:49 +0200

    Various fixes

---
+++ js/fetcher.js
@@ -0,0 +1,10 @@
+(function(){
+    fetch('/html/templates/footer.html').then(response => response.text()).then(footer => {
+        $('#footer').append(footer);
+    });
+    fetch('/html/templates/title.html').then(response => response.text()).then(title => {
+        fetch('/html/templates/navbar.html').then(res => res.text()).then(navbar => {
+            $('#top').append([title, navbar]);
+        });
+    });
+})();
