ff61abd5e836225c30eb1e2ac63abffb970e6af6
Commander <asger.finding@gmail.com>
2021-08-10 15:39:37 +0200

    Removed html folder

--- js/fetcher.js
+++ js/fetcher.js
@@ -1,9 +1,9 @@
 (function(){
-    fetch('html/templates/footer.html').then(response => response.text()).then(footer => {
+    fetch('templates/footer.html').then(response => response.text()).then(footer => {
         $('#footer').append(footer);
     });
-    fetch('html/templates/title.html').then(response => response.text()).then(title => {
-        fetch('html/templates/navbar.html').then(res => res.text()).then(navbar => {
+    fetch('templates/title.html').then(response => response.text()).then(title => {
+        fetch('templates/navbar.html').then(res => res.text()).then(navbar => {
             $('#top').after(navbar);
             $('#top').append(title);
         });
