988da423bc620a805644578401f9b57bea844d7b
Commander <asger.finding@gmail.com>
2021-08-10 15:29:30 +0200

    Fix attempt 3

--- js/fetcher.js
+++ js/fetcher.js
@@ -1,14 +1,9 @@
 (function(){
-    /*
-    if (window.location.host.endsWith('github.io')) {
-
-    }
-    */
-    fetch('..html/templates/footer.html').then(response => response.text()).then(footer => {
+    fetch('html/templates/footer.html').then(response => response.text()).then(footer => {
         $('#footer').append(footer);
     });
-    fetch('..html/templates/title.html').then(response => response.text()).then(title => {
-        fetch('..html/templates/navbar.html').then(res => res.text()).then(navbar => {
+    fetch('html/templates/title.html').then(response => response.text()).then(title => {
+        fetch('html/templates/navbar.html').then(res => res.text()).then(navbar => {
             $('#top').after(navbar);
             $('#top').append(title);
         });
