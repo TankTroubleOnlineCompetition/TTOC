1792425b216fd00b7a52ab88d55de5654b1ab7b5
Commander <asger.finding@gmail.com>
2021-08-11 23:11:33 +0200

    Changed indentation from spaces to tabs. Better for people who have custom settings for indents. Un-crossed requirements in signup because the Signup form is visisble anyways.

--- js/fetcher.js
+++ js/fetcher.js
@@ -1,13 +1,13 @@
 (function () {
-    document.addEventListener('contextmenu', event => event.preventDefault());
+	document.addEventListener('contextmenu', event => event.preventDefault());
 
-    fetch('templates/footer.html').then(response => response.text()).then(footer => {
-        $('#footer').append(footer);
-    });
-    fetch('templates/title.html').then(response => response.text()).then(title => {
-        fetch('templates/navbar.html').then(res => res.text()).then(navbar => {
-            $('#top').after(navbar);
-            $('#top').append(title);
-        });
-    });
+	fetch('templates/footer.html').then(response => response.text()).then(footer => {
+		$('#footer').append(footer);
+	});
+	fetch('templates/title.html').then(response => response.text()).then(title => {
+		fetch('templates/navbar.html').then(res => res.text()).then(navbar => {
+			$('#top').after(navbar);
+			$('#top').append(title);
+		});
+	});
 })();
