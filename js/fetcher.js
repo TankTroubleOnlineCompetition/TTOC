447f77e6a23a4e1a344e25c582830618f36bd850
Commander <asger.finding@gmail.com>
2021-08-12 14:09:29 +0200

    Small rewrite in js/fetcher.js

--- js/fetcher.js
+++ js/fetcher.js
@@ -6,8 +6,7 @@
 	});
 	fetch('templates/title.html').then(response => response.text()).then(title => {
 		fetch('templates/navbar.html').then(res => res.text()).then(navbar => {
-			$('#top').after(navbar);
-			$('#top').append(title);
+			$('#top').after(navbar).append(title);
 		});
 	});
 })();
